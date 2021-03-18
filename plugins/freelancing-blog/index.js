d850c8675c6064279673813347b81ca154b3c352
Pat Allan <pat@freelancing-gods.com>
2021-03-18 11:19:35 +1100

    Tweak build order.
    We want TS docs in place before the site is updated, but we also don't want the search data including TS docs.

--- plugins/freelancing-blog/index.js
+++ plugins/freelancing-blog/index.js
@@ -1,10 +1,11 @@
 module.exports = {
   async onPreBuild({ utils: { run } }) {
     await run.command('bundle exec rake tags')
+    await run.command('bundle exec drumknott refresh')
+    await run.command('bin/load_remotes')
   },
 
   async onSuccess({ utils: { run } }) {
-    await run.command('bundle exec drumknott refresh')
-    await run.command('bin/load_remotes')
+    //
   },
 }
